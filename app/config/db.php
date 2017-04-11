367420b260cb09949baf17727ca8b856c676e143
Vladislav Estraih <estraikh_v@meshalkin.ru>
2017-04-11 19:00:39 +0700

    init

---
+++ app/config/db.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=sonkotmb',
+    'username' => 'root',
+    'password' => 'viesha66',
+    'charset' => 'utf8',
+    'tablePrefix' => '',
+    'enableSchemaCache' => true,
+];
