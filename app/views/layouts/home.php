c518048463c95d0f89a1c850aa7ee10653f516a2
Vladislav Estraikh <winterwar@yandex.ru>
2017-05-04 21:55:23 +0700

    upd

--- app/views/layouts/home.php
+++ app/views/layouts/home.php
@@ -193,13 +193,13 @@ $asset = \app\assets\AppAsset::register($this);
                 <!-- Модуль правого меню -->
                 <div class="module-right-menu">
                     <ul>
-                        <li><a href="/poetapnyy.html">Поэтапный доступ НКО к бюджетным средствам</a></li>
-                        <li><a href="#">Конкурсы НКО</a></li>
-                        <li><a href="#">Фестиваль "Добрый Тамбов"</a></li>
-                        <li><a href="/proekt.html">Лучшие практики</a></li>
-                        <li><a href="/otchety.html">Отчёты НКО</a></li>
-                        <li><a href="#">Библиотека НКО</a></li>
-                        <li><a href="#">Правовая база</a></li>
+                        <li><a href="#">Поэтапный доступ НКО к бюджетным средствам</a></li>
+                        <li><a href="/site/category/6">Конкурсы НКО</a></li>
+                        <li><a href="/site/category/9">Фестиваль "Добрый Тамбов"</a></li>
+                        <li><a href="#">Лучшие практики</a></li>
+                        <li><a href="#">Отчёты НКО</a></li>
+                        <li><a href="/site/page/3">Библиотека НКО</a></li>
+                        <li><a href="/site/page/2">Правовая база</a></li>
                     </ul>
                 </div>
                 <!-- Модуль какого-то виджета -->
