d5fbc84ba860a0bb2130812d1ddf0367107656c1
Vladislav Estraikh <winterwar@yandex.ru>
2017-05-25 02:10:40 +0700

    steplink

--- app/views/site/rightmenu.php
+++ app/views/site/rightmenu.php
@@ -1,7 +1,7 @@
 <!-- Модуль правого меню -->
 <div class="module-right-menu">
     <ul>
-        <li><a href="#">Поэтапный доступ НКО к бюджетным средствам</a></li>
+        <li><a href="/site/stepbystep">Поэтапный доступ НКО к бюджетным средствам</a></li>
         <li><a href="/site/tree/6">Конкурсы НКО</a></li>
         <li><a href="/site/category/9">Фестиваль "Добрый Тамбов"</a></li>
         <li><a href="/site/category/13">Лучшие практики</a></li>
