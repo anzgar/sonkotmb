9bf083463ac1dfb64a245d0b8af376d227271aa0
Vladislav Estraih <estraikh_v@meshalkin.ru>
2017-08-03 18:44:51 +0700

    uptolike

--- app/views/layouts/main.php
+++ app/views/layouts/main.php
@@ -117,6 +117,19 @@ $asset = \app\assets\AppAsset::register($this);
         <div class="template-position">
             <?= $content ?>
         </div>
+        <?php if ($_SERVER['REQUEST_URI'] != '/site/idea' && $_SERVER['REQUEST_URI'] != '/site/takepart') : ?>
+        <script type="text/javascript">(function(w,doc) {
+                if (!w.__utlWdgt ) {
+                    w.__utlWdgt = true;
+                    var d = doc, s = d.createElement('script'), g = 'getElementsByTagName';
+                    s.type = 'text/javascript'; s.charset='UTF-8'; s.async = true;
+                    s.src = ('https:' == w.location.protocol ? 'https' : 'http')  + '://w.uptolike.com/widgets/v1/uptolike.js';
+                    var h=d[g]('body')[0];
+                    h.appendChild(s);
+                }})(window,document);
+            </script>
+            <div style="position:relative;top:10px;left:310px;width:50%;" data-background-alpha="0.0" data-buttons-color="#FFFFFF" data-counter-background-color="#ffffff" data-share-counter-size="12" data-top-button="false" data-share-counter-type="common" data-share-style="1" data-mode="share" data-like-text-enable="false" data-mobile-view="true" data-icon-color="#ffffff" data-orientation="horizontal" data-text-color="#000000" data-share-shape="round-rectangle" data-sn-ids="fb.vk.tw.ok." data-share-size="30" data-background-color="#ffffff" data-preview-mobile="false" data-mobile-sn-ids="fb.vk.tw.ok.wh.vb.tm." data-pid="1692522" data-counter-background-alpha="1.0" data-following-enable="false" data-exclude-show-more="false" data-selection-enable="true" class="uptolike-buttons" ></div>
+            <?php endif; ?>
     </div>
 
     <!-- Позиция футера -->
